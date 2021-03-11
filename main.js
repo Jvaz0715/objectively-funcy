e4ea62d7fd6546487634bc4f3b46488728123330
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-11 14:40:52 -0500

    Solves giveBirthday Function

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function getLastName(object) {
 // console.log(getLastName({firstName: 'Colin', lastName: 'Jaffe'}))
 
 // getFullName
+// returns the full name
 
 function getFullName(object) {
   let first = object.firstName;
@@ -27,8 +28,36 @@ function getFullName(object) {
   return `${first} ${last}`
 }
 
+// setFirstName
+// changes the value of the first name of given object to given value
 
+function setFirstName(object, value) {
+  return object.firstName = value;
+}
 
+// setAge
+// changes the value of the age property of the given person object to the given value
+
+function setAge(object, value) {
+  return object.age = value;
+}
+
+//console.log(setAge({firstName: 'Colin', lastName: 'Jaffe', age: 39}, 45))
+
+// giveBirthday
+// increments the age property of the given person object, or gives them an age of 1 if they don't have that property
+
+function giveBirthday(object) {
+
+  if (object.age === undefined) {
+    return object.age = 1;
+  } else {
+    return object.age += 1;
+  }
+}
+
+console.log(giveBirthday({firstName: 'Colin', lastName: 'Jaffe', age: 39}))
+console.log(giveBirthday({firstName: 'Baby', lastName: 'Jaffe'}))
 
 
 
